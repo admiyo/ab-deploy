232325ee882ae35ae42bf42d83930dc685a9a7b3
Adam Young <adam@younglogic.com>
2020-01-21 14:11:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php 
  echo "I am VERSION 2 Written on the fly!<br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] \n;
  echo "<br><br>"
  ?>
