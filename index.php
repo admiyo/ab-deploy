0c12a2575c9337920e0fc69b7a69171d2148b501
Adam Young <adam@younglogic.com>
2020-01-28 17:54:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
  <?php 
- echo "I am VERSION 2 Written on the fly!<br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'];
- echo "<br><br>\n";
- echo "My Uid is : ".getmyuid() . "\n";
- echo  "My euid is : " .posix_geteuid() . "\n";
+ echo "I am VERSION 3 Written on the fly!<br>";
+ echo "<br>My Pod IP is : ".$_SERVER['SERVER_ADDR']."\n";
+ echo "<br>My Uid is : ".getmyuid() . "\n";
+ echo "<br>My euid is : " .posix_geteuid() . "\n";
  ?>
